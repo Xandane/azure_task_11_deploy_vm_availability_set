dc5bfa447bc72a9855785bf99fafd3632ee7a635
Yuriy Mishchuk <yuramishchuk1703@gmail.com>
2026-05-23 00:31:11 +0300

    fixed resgroup

--- task.ps1
+++ task.ps1
@@ -1,5 +1,5 @@
 $location = "canadacentral"
-$resourceGroupName = "mate-azure-task-11"
+$resourceGroupName = "mate-resources"
 $networkSecurityGroupName = "defaultnsg"
 $virtualNetworkName = "vnet"
 $subnetName = "default"
